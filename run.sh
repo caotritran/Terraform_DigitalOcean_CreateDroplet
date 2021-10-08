bc04c52bf7ea7c29ae23fcecf5fae77e3529064f
caotritran <caotritran.14@gmail.com>
2021-10-08 23:57:22 +0700

    update script run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 sudo rm -rf .terraform/ terraform.tfstate terraform.tfstate.backup .terraform.lock.hcl
-sudo terraform init
+terraform init
 
-sudo terraform apply --auto-approve
+terraform apply --auto-approve
 
 
 ip=`terraform output -raw PublicIP`
@@ -11,4 +11,4 @@ pass=`terraform output -json password | jq -r '.[0]'`
 
 echo "deploy_user ansible_host=${ip} ansible_ssh_pass=\"${pass}\" ansible_user=root ansible_port=22" > ./ansible_adduser/iplist.txt
 
-sudo ansible-playbook -i ./Ansible_addUser/iplist.txt ./Ansible_addUser/main.yml
+ansible-playbook -i ./Ansible_addUser/iplist.txt ./Ansible_addUser/main.yml
