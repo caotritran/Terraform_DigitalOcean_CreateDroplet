50e1cbf6bfb0e301fa30689f5a002fe08c99c0cd
caotritran <caotritran.14@gmail.com>
2021-10-09 08:53:02 +0700

    update sshkey config

--- main.tf
+++ main.tf
@@ -4,11 +4,15 @@ resource "random_string" "random" {
   #override_special = "/@£$"
 }
 
-resource "digitalocean_ssh_key" "rootkey" {
-  name = "rootkey"
-  public_key = "KEY_HERE"
+#resource "digitalocean_ssh_key" "rootkey" {
+#  name = "rootkey"
+#  public_key = "KEY_HERE"
   #public_key = file("/opt/terraform/id_rsa.pub")
   #public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCiNdgoKnX13LipucnX1jXk/YFvmtgTlHmDGqvJZ9Pfv+EIKt3LlCdYIMSle6+QgGK1QcmNLqSUaACIsRvUAWUBC+yS5mY3GiC6p9I/GAdNdg5omVtA0dabsgcbQixnrSQqxvq+WgtmEMBXmeKM8fmIZ4nyRH2VD49r/iC+Bnc9b7uDafnff37wZi6bqG55wVWOLA88mZvqYAzcq5ccd1wV014zTVCeEY1tEgM4u+jYw1W/fc0U2gxViaJ0fPFrBxdKUXPUgNi6VTZbDb0C0cE4GVAstOl5jBsqBvdnQykHpOjqfON2KV2g2WBdQRaM2OWOoJVOaEhZzy8Z2rLI+W55"
+#}
+
+data "digitalocean_ssh_key" "rootkey" {
+  name = "rootkey"
 }
 
 resource "digitalocean_droplet" "droplet-1" {
@@ -21,7 +25,8 @@ resource "digitalocean_droplet" "droplet-1" {
   monitoring         = var.do_monitoring
   ipv6               = var.do_ipv6
   vpc_uuid           = var.do_vpc_uuid == "" ? null : var.do_vpc_uuid
-  ssh_keys           = [digitalocean_ssh_key.rootkey.fingerprint]
+  #ssh_keys           = [digitalocean_ssh_key.rootkey.fingerprint]
+  ssh_keys           = [data.digitalocean_ssh_key.rootkey.id]
 
 provisioner "remote-exec" {
      inline = [
